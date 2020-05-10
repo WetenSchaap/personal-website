0eb23623752e65f8104edf061f05d541eccb7cdf
WetenSchaap <git@swnkls.nl>
2020-05-10 15:53:45 +0200

    many changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,16 +8,28 @@ SITETITLE = 'Piet Swinkels'
 SITESUBTITLE = 'My personal website'
 SITEDESCRIPTION = "Piet's Personal Website"
 #SITEURL = "https://www.swnkls.nl"
+SITELOGO = "images/piet.png"
 FAVICON = 'images/favicon.ico'
+LINKS_IN_NEW_TAB = 'external' 
+PATH = 'content'
+DEFAULT_PAGINATION = False
+
+#copyright
+#CC_LICENSE = {
+#    "name": "Creative Commons Attribution-ShareAlike",
+#    "version": "4.0",
+#    "slug": "by-sa"
+#}
+COPYRIGHT_NAME = "Piet Swinkels"
+COPYRIGHT_YEAR = "2020"
+
 
 # colorscheme
 THEME_COLOR = 'dark'
-THEME_COLOR_ENABLE_USER_OVERRIDE = True
+#THEME_COLOR_ENABLE_USER_OVERRIDE = True
 
-PATH = 'content'
-
+# localization
 TIMEZONE = 'Europe/Amsterdam'
-
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -27,40 +39,30 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Ignore cache for dev
+LOAD_CONTENT_CACHE = False
 
 # What to show on the sidebar:
-
 DISPLAY_PAGES_ON_MENU = False
 #DISPLAY_CATEGORIES_ON_MENU = False
 
-MENUITEMS = (("About me", '/about-me.html'),
+MENUITEMS = (("About me", '{filename}/content/pages/about-me.md'),
+             ("Research", '{filename}/content/pages/research.md'),
             )
 
-LINKS = (('NAS', 'https://nas.swnkls.nl'),
-         ('Wiki', 'https://wiki.swnkls.nl'),
-         ('Jellyfin', 'https://jellyfin.swnkls.nl'),
-         ('Deluge', 'https://deluge.swnkls.nl'),
-         ('Git', 'https://git.swnkls.nl'),
-         ('Nginx', 'https://nginx.swnkls.nl'),
-         ('Shaarli', 'https://shaarli.swnkls.nl'),
+LINKS = (('test','https:\\duck.go'),
          )
 
-SOCIAL = (("linkedin", "https://www.linkedin.com/in/pjm-swinkels/"),
-          ("orcid", "https://orcid.org/0000-0002-6118-9746"),
-          ("fa-server", "https://nas.swnkls.nl"),
-          ("git", "https://git.swnkls.nl"),
-          ("cogs", "https://nginx.swnkls.nl"),
-          ("tv", "https://jellyfin.swnkls.nl"),
-          ("cloud-download-alt", "https://deluge.swnkls.nl"),
-          ("wikipedia-w", "https://wiki.swnkls.nl"),
-          ("bookmark", "https://shaarli.swnkls.nl"),
+SOCIAL = (("lab la-linkedin", "https://www.linkedin.com/in/pjm-swinkels/"),
+          ("lab la-orcid", "https://orcid.org/0000-0002-6118-9746"),
+          ("las la-server", "https://nas.swnkls.nl"),
+          ("lab la-git-square", "https://git.swnkls.nl"),
+          ("las la-cogs", "https://nginx.swnkls.nl"),
+          ("las la-tv", "https://jellyfin.swnkls.nl"),
+          ("las la-cloud-download-alt", "https://deluge.swnkls.nl"),
+          ("lab la-wikipedia-w", "https://wiki.swnkls.nl"),
+          ("las la-bookmark", "https://shaarli.swnkls.nl"),
          )
-
-# Social widget
-#SOCIAL = (('ORCID', 'https://orcid.org/0000-0002-6118-9746'),
-#          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
